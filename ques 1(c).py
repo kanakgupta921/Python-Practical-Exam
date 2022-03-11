d4278e66dacd60ff00b0798503e483daa6d86faa
kanakgupta921 <101341094+kanakgupta921@users.noreply.github.com>
2022-03-11 12:53:19 +0530

    Add files via upload

---
+++ ques 1(c).py
@@ -0,0 +1,11 @@
+#c) 1, 2, 4, 3, 5, 7, 9, 6, 8, 10, 11, 13.. till n-th term
+num =int(input("enter"))
+if num < 0:
+    print("Enter a positive number")
+else:
+    sum = 0
+   
+while(num > 0):
+    sum += num
+    num -= 1
+print("The sum is", sum)
